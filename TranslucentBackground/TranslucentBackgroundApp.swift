0e27dafac8a937ced690f034ec0effa435eed3e6
Nick Rice <nick.rice@starlingbank.com>
2023-04-03 19:20:23 +0100

    Initial Commit

---
+++ TranslucentBackground/TranslucentBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslucentBackgroundApp.swift
+//  TranslucentBackground
+//
+//  Created by Nick Rice on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TranslucentBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
